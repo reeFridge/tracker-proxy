72ab17ef707ab07ee59bb2f373de84b29d1d8384
reeFridge <reefridgerator@gmail.com>
2016-06-18 14:17:08 +0700

    Rewrote to ES6

--- src/interfaces/i-tracker.js
+++ src/interfaces/i-tracker.js
@@ -1,41 +1,33 @@
-
-
-
 /**
  * Common torrent-tracker interface.
  * @interface
  */
-ITracker = function() {};
+class ITracker {
+  /**
+   * Search query.
+   * @param {QueryParams=} optParams
+   * @return {Promise.<IQueryResponse|error>}
+   */
+  getQuery(optParams) {}
 
+  /**
+   * Tracker availability check.
+   * @return {Promise.<undefined|error>}
+   */
+  isAvailable() {}
 
-/**
- * Search query.
- * @param {QueryParams=} opt_params
- * @return {Promise.<IQueryResponse|error>}
- */
-ITracker.prototype.getQuery = function(opt_params) {};
-
-
-/**
- * Tracker availability check.
- * @return {Promise.<undefined|error>}
- */
-ITracker.prototype.isAvailable = function() {};
-
-
-/**
- * Get tracker url.
- * @return {string}
- */
-ITracker.prototype.getUrl = function() {};
-
+  /**
+   * Get tracker url.
+   * @return {string}
+   */
+  getUrl() {}
+}
 
 /**
- * @typedef {{
- *     query: string,
- *     category: string,
- *     maxItems: number,
- *     order: string,
- *     desc: boolean
- * }} QueryParams
+ * @typedef {Object} QueryParams
+ * @property {string} query Search query
+ * @property {string} category Search category
+ * @property {number} maxItems Max items in the response
+ * @property {string} order Sort order
+ * @property {boolean} desc Descending order if true
  */
